7d41f9d30bc2d5099de49bb3509f42e35a749b35
Doom_hopes <diadia1911@ukr.net>
2020-12-13 10:23:04 +0200

    add tank

--- lib/my_flexible.dart
+++ lib/my_flexible.dart
@@ -7,18 +7,47 @@ class MyFlexible extends StatelessWidget {
       appBar: AppBar(title: Text('RowColumn')),
       body: Container(
         color: Colors.grey,
-        child: Row(
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            BiggerColorBox(),
+            Container(
+              width: 60,
+              child: Text(
+                'T-34-85',
+                style: TextStyle(fontSize: 14, color: Colors.black87),
+                softWrap: true, // Текст не вылазит за границы родителя
+                overflow:
+                    TextOverflow.visible, // Если текст не влез, то он исчезает
+              ),
+            ),
+            Icon(
+              Icons.favorite,
+              size: 60,
+              color: Colors.red,
+            ),
+            Image.network(
+                'https://www.southwarknews.co.uk/wp-content/uploads/2020/04/Bermondsey-tank.jpg'),
+
+            /*BiggerColorBox(),
             Flexible(
                 fit: FlexFit
                     .tight, //FlexFit показывает как элемент будет занимать все доступное пространство
                 flex:
                     2, //flex показывает долю которыю забирает себе каждый гибкий виджет
                 child: ColorBox()),
+
+            /*SizedBox(
+              width: 40,
+            ),
+
+            SizedBox(width: 100, height: 100, child: ColorBox()),*/
+
+            Spacer(
+              flex: 1,
+            ),
+
             Expanded(flex: 4, child: ColorBox()),
-            //Expanded(child: null) // тот же Flexible где fit = tight
+            //Expanded(child: null) // тот же Flexible где fit = tight*/
           ],
         ),
       ),
